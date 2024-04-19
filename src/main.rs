52d3d878d663fc2fb5a72edd41dd02f146c9cfd1
kevinbednarek <kevinbednarek@gmail.com>
2024-04-19 15:48:03 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use gloo_net::http::Request;
+use yew::prelude::*;
+use yew_app::{Video, VideosList, VideoDetails};
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let videos = use_state(|| vec![]);
+        {
+            let videos = videos.clone();
+            use_effect_with((), move |_| {
+                let videos = videos.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    videos.set(fetched_videos);
+                });
+                || ()
+            });
+        }
+
+    let selected_video = use_state(|| None);
+
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| {
+            selected_video.set(Some(video))
+        })
+    };
+
+    let details = selected_video.as_ref().map(|video| html! {
+        <VideoDetails video={video.clone()}/>
+    });
+
+    html! {
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
+            </div>
+            {for details}
+            <div>
+                <h3>{ "John Doe: Building and breaking things" }</h3>
+                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+            </div>
+        </>
+    }
+}
+
+
+
